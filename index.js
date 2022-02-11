699a75c3b923d962faad9e4cd449c0a729b95292
Siddhant Kunde <46617967+siddhantkunde@users.noreply.github.com>
2022-02-11 17:53:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.get("/api",(req,res) =>{
+    res.json({
+      message:"Hey Welcome to API"
+  });
+});
+
+app.post("/api/posts",verifyToken,(req,res)=>{
+    jwt.verify(req.token,'secretkey',(err,authData)=>{
+        if(err){
+            //res.sendStatus(403);
+            console.log(err);
+        }else{
+            res.json({
+                message:'post created',
+                authData,
+            });
+        }
+    });
+    
+});
+
+app.post('/api/login',(req,res)=>{
+    const user = {
+        id:1,
+        username:"John",
+        email:"john@gmail.com",
+    };
+    jwt.sign({user:user},"secretkey",(err,token)=>{
+        res.json({
+            token,
+        });
+    });
+});
+
+function verifyToken(req,res,next){
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== "undefined"){
+
+        const bearerToken = bearerHeader.split(" ")[1];
+        req.token=bearerToken;
+        next();
+
+    }
+    else{
+        res.sendStatus(403)//forbidden
+    }
+
+}
+
+app.listen(3000,(req,res)=> {
+    console.log('server started on 3000')
+})
